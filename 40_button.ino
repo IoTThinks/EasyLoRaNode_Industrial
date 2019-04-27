5b82fe124d306e615c666d116cd566566f5ef1a4
IoTThinks.com <vinhdat82@gmail.com>
2019-04-27 16:28:08 +0700

    Support sensor and actuator
    Able to send gas sensor information to LoRa Gateway and activate 220v light with SSR.

---
+++ 40_button.ino
@@ -0,0 +1,10 @@
+void setupButton()
+{
+  pinMode(BTN, INPUT);       // sets the digital pin as input
+}
+
+void buttonPressed()
+{
+
+}
+
---
+++ 50_led.ino
@@ -0,0 +1,74 @@
+// ====================================
+// LED
+// ====================================
+
+String LED_Status = "Not initalized";
+
+void setupLED() {
+  pinMode(LED1, OUTPUT);    
+  digitalWrite(LED1, LOW);  
+
+  pinMode(LED2, OUTPUT);    
+  digitalWrite(LED2, LOW);  
+
+  pinMode(LED3, OUTPUT);    
+  digitalWrite(LED3, LOW);  
+
+  pinMode(LED4, OUTPUT);    
+  digitalWrite(LED4, LOW);  
+  
+  LED_Status = "OK";
+  Serial.println("[LED] Setup LED");
+}
+
+// LED 1
+void onLED1() {
+  // Already ON, to skip
+  if(digitalRead(LED1) == HIGH)
+    return;
+  
+  Serial.println("[LED] On LED1");
+  digitalWrite(LED1, HIGH);
+}
+
+void offLED1() {  
+  // Already OFF, to skip
+  if(digitalRead(LED1) == LOW)
+    return;
+  
+  Serial.println("[LED] Off LED1");
+  digitalWrite(LED1, LOW);
+}
+
+// LED 2
+void onLED2() {
+  Serial.println("[LED] On LED2");
+  digitalWrite(LED2, HIGH);
+}
+
+void offLED2() {  
+  Serial.println("[LED] Off LED2");
+  digitalWrite(LED2, LOW);
+}
+
+// LED 3
+void onLED3() {
+  Serial.println("[LED] On LED3");
+  digitalWrite(LED3, HIGH);
+}
+
+void offLED3() {  
+  Serial.println("[LED] Off LED3");
+  digitalWrite(LED3, LOW);
+}
+
+// LED 4
+void onLED4() {
+  Serial.println("[LED] On LED4");
+  digitalWrite(LED4, HIGH);
+}
+
+void offLED4() {  
+  Serial.println("[LED] Off LED4");
+  digitalWrite(LED4, LOW);
+}
